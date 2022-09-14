b87235205bfe0ad3bdd02e5a5bb73e3a32302aa6
HgaMen <hga.int2021@gmail.com>
2022-09-14 13:17:42 -0500

    dia 2

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,17 @@
 <script setup>
-import axios from 'axios';
-import { ref } from 'vue';
-
-const dolar = ref('');
-const uf = ref('');
-const fecha = ref('');
-const loading = ref(true);
-
-const getData = async () => {
-  loading.value = true;
-  try {
-    const { data } = await axios.get('https://api-dolar-v26b.onrender.com/');
-    // console.log(data);
-    dolar.value = data.dolar;
-    uf.value = data.UF;
-    fecha.value = data.fecha;
-  } catch (error) {
-    console.log(error);
-  } finally {
-    loading.value = false;
-  }
-};
-getData();
+import CardMoney from '@/components/CardMoney.vue';
+import NavBar from '@/components/NavBar.vue';
 </script>
 
 <template>
-  <h1 v-if="loading">Loading data...</h1>
-  <h1>Api Dolar Fecha: {{ fecha }} - Dolar: {{ dolar }} - UF: {{ uf }}</h1>
+  <NavBar />
+  <h1 class="font-light text-center text-blue-600">API DOLAR</h1>
+  <div class="container px-4 mx-auto">
+    <main class="grid grid-cols-1 gap-2 pt-5 sm:grid-cols-4">
+      <CardMoney />
+      <CardMoney />
+      <CardMoney />
+      <CardMoney />
+    </main>
+  </div>
 </template>
